dfcc0e22972cedea299248702111eeda55b258dc
Tim <timcash@gmail.com>
2013-12-19 16:42:57 -0800

    adding sudo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo -i
-mv oakleon.service /media/state/units
-systemctl daemon-reload
-systemctl restart local-enable.service
+
+sudo mv oakleon.service /media/state/units
+sudo systemctl daemon-reload
+sudo systemctl restart local-enable.service
 docker ps
